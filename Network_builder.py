c1d99fd4569e826aaef89d0bc9f351b36dddba8a
chiffa <andrei.chiffa136@gmail.com>
2017-10-20 18:29:47 -0400

    condition-specific genes and robustness verification

--- Network_builder.py
+++ Network_builder.py
@@ -1,7 +1,7 @@
 import pickle
 import random
 from itertools import product, combinations
-from scipy.stats import gaussian_kde, norm
+from scipy.stats import gaussian_kde, norm, chi
 from matplotlib import pyplot as plt
 
 import numpy as np
@@ -10,12 +10,12 @@ essentiality = 0.3  # epsilon - if pathway throughput falls below this value we 
 aneup_essentiality = 0.4  # espilon for aneuploid pathway
 min_length = 1.99  # minimal length of the pathway
 total_non_essential_pool = 5400
-independent_pathways = 19  # estimation of independent pathways in yeast
+independent_pathways = 17  # estimation of independent pathways in yeast
+stress_conditions = 50
 
-length_width = pickle.load(open('w_l_accumulator.dmp', 'rb'))
-activations = pickle.load(open('activations.dmp', 'rb'))
-aneup_perturbation = pickle.load(open('norm_aneup.dmp', 'rb'))
-
+length_width = pickle.load(open('w_l_accumulator.dmp', 'r'))
+activations = pickle.load(open('activations.dmp', 'r'))
+aneup_perturbation = pickle.load(open('norm_aneup.dmp', 'r'))
 aneup_perturbation = aneup_perturbation.flatten()
 
 length_width = np.array(length_width)
@@ -25,10 +25,32 @@ _width = np.array(length_width)[:, 1]
 _width = _width[_length > min_length]
 _length = _length[_length > min_length]
 
-length_width = np.column_stack((_length, _width))
 
 activations = np.array(activations).flatten()
 activations = activations[np.logical_not(np.isnan(activations))]
+
+# ## uncomment if we want to have random perturbations:
+# essentiality = 0.5
+# aneup_essentiality = 0.6
+#
+# independent_pathways = 50
+#
+# f_length = np.random.rand(*_length.shape)
+# f_length *= np.max(_length)
+# f_length += 2.
+#
+# f_width = np.random.rand(*_width.shape)
+# f_width *= np.max(_width)
+# f_width += 1.
+#
+# _length = f_length
+# _width = f_width
+#
+# activations = np.random.randn(*activations.shape)
+# aneup_perturbation = np.random.lognormal(0, 0.5, *aneup_perturbation.shape)
+##
+
+length_width = np.column_stack((_length, _width))
 
 # checking if gaussian hump plays a role:
 # activations[np.logical_and(activations < 1.0, activations > -1.0)] = 0  # Nope, percolation....
@@ -43,6 +65,7 @@ def simulation_run():
     total_interactions = 0
 
     cond_essentail_genes = 0
+    genes_essential_in_cond = 0
 
     for _ in range(0, 100):
         _l, _w = random.choice(length_width.tolist())
@@ -112,6 +135,22 @@ def simulation_run():
                 # print '<<<<'
                 cond_essentail_genes += 1
 
+        for j, i in non_essentials:
+            pad[j, i] = 0
+
+            flag = False
+            for _ in range(0, stress_conditions):
+                signal = (np.random.rand(_w, 1) + 0.5) / float(_w)
+                for k, mat in enumerate(abs_m_chain):
+                    signal = np.dot(mat, signal)
+                    signal = signal * np.expand_dims(pad[k, :], 1)
+                perturbed = np.sum(signal)
+                if perturbed / unperturbed < essentiality:
+                    flag = True
+            if flag:
+                genes_essential_in_cond += 1
+
+
         for gene_1, gene_2 in combinations(non_essentials, 2):
             pad = np.ones((_l, _w))
             pad[gene_1[0], gene_1[1]] = 0
@@ -140,9 +179,11 @@ def simulation_run():
     print 'current score:', float(essential_genes) / float(total_genes)
     print 'synth lethals:', float(synthetic_lethals) / float(total_interactions)
     print 'essentials no more after aneuploidy', float(cond_essentail_genes)/float(essential_genes)
+    print 'genes essential in condition', float(genes_essential_in_cond)/float(total_genes - essential_genes)
     return float(essential_genes) / float(total_genes),\
            float(synthetic_lethals) / float(total_interactions),\
-           float(cond_essentail_genes)/float(essential_genes)
+           float(cond_essentail_genes)/float(essential_genes),\
+           float(genes_essential_in_cond)/float(total_genes - essential_genes)
 
 
 def improved_plot(data, stats_of_interest, x_axis):
@@ -166,12 +207,15 @@ if __name__ == "__main__":
     essentials_accumulator = []
     lethal_accumulator = []
     conditional_essentials = []
+    ess_in_cond = []
+
 
     for _ in range(0, 25):
-        essentials_fraction, lethal_int_fraction, cond_ess_fraction = simulation_run()
+        essentials_fraction, lethal_int_fraction, cond_ess_fraction, ess_in_cond_fraction = simulation_run()
         essentials_accumulator.append(essentials_fraction)
         lethal_accumulator.append(lethal_int_fraction)
         conditional_essentials.append(cond_ess_fraction)
+        ess_in_cond.append(ess_in_cond_fraction)
 
     improved_plot(np.array(essentials_accumulator)*100,
                   'essential genes prevalence',
@@ -182,5 +226,6 @@ if __name__ == "__main__":
     improved_plot(np.array(conditional_essentials)*100,
                   'evolvable essential genes',
                   'percentage of all essential genes')
-
-
+    improved_plot(np.array(ess_in_cond)*100,
+                  'condition-specific essential in at least 1 out of 50 conditions',
+                  'percentage of all non-essential genes')
